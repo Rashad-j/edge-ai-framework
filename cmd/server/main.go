f9eb5c7a58830b2c1c7e51104b4293bae6a7bbcf
Rashad <s8raalbo@stud.uni-saarland.de>
2024-11-20 14:56:33 +0100

    chore(logger): delete unessary logger logic and introduce zerolog

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,2 +1,20 @@
 package main
 
+import (
+	"github.com/Rashad-j/edge-ai-framework/pkg/config"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"os"
+)
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "2006-01-02 15:04:05"})
+
+	// load config
+	_, err := config.ReadEnvConfig()
+	if err != nil {
+		log.Fatal().Err(err).Msg("read env config failed")
+	}
+
+}
